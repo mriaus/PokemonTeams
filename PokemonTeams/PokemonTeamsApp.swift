1861ed0e90f1903c731d0c1321d5936351137594
mriaus <55255215+mriaus@users.noreply.github.com>
2024-05-07 10:11:13 +0200

    Initial Commit

---
+++ PokemonTeams/PokemonTeamsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonTeamsApp.swift
+//  PokemonTeams
+//
+//  Created by Marcos on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonTeamsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
